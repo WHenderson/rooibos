8e5c4c0828b62613a523a9a441407d61a028144c
WHenderson <whenderson.github+smartgit@gmail.com>
2020-03-29 20:39:30 +1100

    Cleanup

--- src/Reporters/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const VerboseReporter_1 = require("./VerboseReporter");
-exports.VerboseReporter = VerboseReporter_1.VerboseReporter;
-const JsonReporter_1 = require("./JsonReporter");
-exports.JsonReporter = JsonReporter_1.JsonReporter;
-const PipeReporter_1 = require("./PipeReporter");
-exports.PipeReporter = PipeReporter_1.PipeReporter;
-//# sourceMappingURL=index.js.map
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Testish_1 = require("./Testish");
-exports.Testish = Testish_1.Testish;
-const Reporters_1 = require("./Reporters");
-exports.VerboseReporter = Reporters_1.VerboseReporter;
-exports.PipeReporter = Reporters_1.PipeReporter;
-exports.JsonReporter = Reporters_1.JsonReporter;
-//# sourceMappingURL=index.js.map
